1636086b62e7a3dd9869dbbae41ffa1ffda04279
Daniel <daniel.petrusevski13@gmail.com>
2018-08-22 12:55:34 +0200

    TOP5 google search Project

---
+++ TOP5/README.txt
@@ -0,0 +1,4 @@
+Project: TOP 5 search results in google
+Libraries: requests, sys, bs4, selenium (webdriver)
+
+Creating a program that open in browser the top 5 search results (on given topic) in Google
---
+++ TOP5/TOP5.py
@@ -0,0 +1,25 @@
+import requests, sys, webbrowser, bs4
+
+print ('Googling...')
+print (sys.argv[1:]) #Enter the arguments in EDIT CONFIGURATIONS
+res = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
+res.raise_for_status()
+
+
+
+
+
+#TODO: Retrieve top search result links
+soup = bs4.BeautifulSoup(res.text, 'html.parser')
+
+
+
+
+
+#TODO: Open a browser tab for each result
+linkElems = soup.select('.r a')
+
+numOpen = min(5, len(linkElems))
+for i in range (numOpen):
+    webbrowser.open('http://google.com/' + linkElems[i].get('href'))
+
